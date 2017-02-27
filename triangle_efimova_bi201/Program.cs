92f70e3a04df3c2eb629924c65b7e424d2904b8d
Никита <@->
2017-02-27 23:16:15 +0500

    Добавьте файлы проекта.

---
+++ triangle_efimova_bi201/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangle_efimova_bi201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
